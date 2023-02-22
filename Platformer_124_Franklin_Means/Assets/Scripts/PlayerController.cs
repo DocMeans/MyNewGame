5482a1c2584f8a90e54e08ca996805947b3a1f8b
DocMeans <75758516+DocMeans@users.noreply.github.com>
2023-02-22 17:44:33 -0500

    small additions
    disabled the double jump and added some more platforms.

--- Platformer_124_Franklin_Means/Assets/Scripts/PlayerController.cs
+++ Platformer_124_Franklin_Means/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private float jumpStrength = 5.0f;
     private float leftAndRightInput;
     private Rigidbody playerRb;
+    public bool isOnGround = true; 
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +28,14 @@ public class PlayerController : MonoBehaviour
         transform.Translate(leftAndRightInput * Time.deltaTime * walkSpeed * Vector3.forward);
 
         // Makes the character jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
         {
-            playerRb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);       
+            playerRb.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+            isOnGround = false;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true; // prevents double jumping. lines 13, 31, 34,and 39
     }
 }
