642794b9ac4d4abf5df35d92458514523d23e3e9
DocMeans <75758516+DocMeans@users.noreply.github.com>
2023-02-26 20:44:55 -0500

    Weekly update

--- Platformer_124_Franklin_Means/Assets/Scripts/PlayerController.cs
+++ Platformer_124_Franklin_Means/Assets/Scripts/PlayerController.cs
@@ -2,28 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class PlayerController : MonoBehaviour
 {
     private float walkSpeed = 5.0f; 
     private float jumpStrength = 5.0f;
-    private float leftAndRightInput;
+    private float leftAndRightInput; 
     private Rigidbody playerRb;
     public bool isOnGround = true; 
-
     // Start is called before the first frame update
     void Start()
-    {
+    {// relabeling the rigidbody component
         playerRb = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
-       //player input
+        //player input
         leftAndRightInput = Input.GetAxis("Horizontal");
-
         // Moves character left or right
         transform.Translate(leftAndRightInput * Time.deltaTime * walkSpeed * Vector3.forward);
 
@@ -36,6 +31,6 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true; // prevents double jumping. lines 13, 31, 34,and 39
+         isOnGround = true; //  lines 1, 26, 29,and 34 together prevents double jumping.
     }
 }
